3dabd284f0145946879521dfb5191aa9881ebe64
Mohamed Elsayed <sasambmb63@gmail.com>
2026-06-29 11:43:33 +0300

    3 commit

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_origins' => ['http://localhost:3000',
+                          'http://127.0.0.1:3000',
+                          'http://192.168.80.1:3000'],
 
     'allowed_origins_patterns' => [],
 
